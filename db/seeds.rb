c5a94ae7643a46d53484cfa434fc8ddbc2f52511
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-18 19:57:50 -0500

    add products to seeds file, inital data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "crayon", price: 3, image_url: "https://www.shutterstock.com/image-photo/red-wax-crayon-isolated-on-260nw-22318996.jpg", description: "for children to write on walls with"},
+  {name: "fire_truck", price: 4, image_url: "https://www.3000toys.com/images/ERTL/46731-CNP.jpg", description: "its loud and red"},
+  {name: "action_figure", price: 8, image_url: "https://ae01.alicdn.com/kf/Sc4521cc7aa7e4d39a058d63ca6693c1fE/Japan-Anime-Dragon-Ball-Z-Original-BANDAI-SPIRITS-S-H-Figuarts-SHF-Exclusive-Action-Figure-Great.jpg", description: "posable super hero"},
+  {name: "legos", price: 9, image_url: "https://www.thebrickfan.com/wp-content/uploads/2022/12/LEGO-Minecraft-The-Sword-Outpost-21244.jpg", description: "build stuff"},
+  {name: "Coloring_book", price: 5, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu4pd2boLF47NDdZsoAwA2rxiUcNsT_51APQ&usqp=CAU", description: "what crayons are supposed to be used on"},
+  {name: "teddy_bear", price: 7, image_url: "https://www.wdtn.com/wp-content/uploads/sites/45/2023/02/what-is-the-history-behind-the-teddy-bear_.jpg?w=760", description: "stuffed doll in shape of bear "},
+  {name: "jump_rope", price: 9, image_url: "https://www.lifefitness.com/resource/blob/2296806/9374e60d493962374b70ca5788d555dc/2022-skipping-rope-blue-data.jpg", description: "cord with handles"},
+  {name: "water_gun", price: 15, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwjoteCH_qxqAFxT_gG0oNsh9jRJBKtX5KVQ&usqp=CAU", description: "it sprays water"},
+  {name: "frisbee ", price: 7, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoHta3UhoHW9w91guIF1Kfng2t-pTz7iWJWA&usqp=CAU", description: "disc made to be thrown"},
+  {name: "foot_ball", price: 12, image_url: "https://gray-wluc-prod.cdn.arcpublishing.com/resizer/TboMcITvzts4CoLU9MAP769cQZU=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/IPRNNKGQJ5CMPNAQCBKBNA7TE4.jpg", description: "ball meant to be thrown not kicked"}
+])
