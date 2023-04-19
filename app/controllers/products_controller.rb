1b59dc3b6e6bd6729d101eaca9e03ca0992d4c0c
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-18 20:55:48 -0500

    add create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end 
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    render :show
+  end
+
 end 
 
