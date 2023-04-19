a074d49163d6f600db6e23d1b53f0f0af3e4c1b5
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-19 08:51:53 -0500

    add taxt,total, and is_discounted?

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    !(price >= 10)
+    # if price > 10
+    #   return false
+    # else
+    #   return true
+    # end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+  
 end
